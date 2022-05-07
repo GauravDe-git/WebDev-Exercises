5ea64a6f94e4204d82fa05dad6a6b8aa6392dd7d
Gaurav De <gm.gauravde@gmail.com>
2022-05-08 03:42:56 +0530

    Feat: Added Min Max qustion

---
+++ BuildLogic/#1OperatorsBranchingLoops/FindMaxAndMin.js
@@ -0,0 +1,12 @@
+// Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
+
+const num1 = 129, num2 = 251;
+
+if (num2 > num1)
+{
+    console.log(`${num2} is greater than ${num1}`)
+}
+else
+{
+    console.log(`${num1} is greater than ${num2}`)
+}
